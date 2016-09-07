3b1888a7d674454b7a5f6b554996f0919d7a0419
Diko Tech Slave <diko316@gmail.com>
2016-09-07 09:25:11 +0800

    created load() method in convention.js added feature to create configurations from a callback a la middleware. very useful for modularizing definitions in CommonJS way. created activity workflow definition where usecase workflow registered will be in this format [system]://[subject]/[usecase]

--- test.js
+++ test.js
@@ -1,29 +1,3 @@
 'use strict';
 
-var USECASE = require('./index.js');
-
-
-
-var app = USECASE('my-system');
-
-app.as('guest').
-        emulating('visitor', 'unknown').
-        accessing('public').
-            can('visit sites').
-                soThat(
-                    'We can show that this works',
-                    'As tested').
-            
-            can('login as admin');
-            
-            
-
-
-app.subject('public').
-            usecase('login').
-                extend('login as admin');
-
-console.log(
-    require('util').
-        inspect(app.definition, { depth: 10, breakLength: 1 })
-);
+require('./test/index.js');
